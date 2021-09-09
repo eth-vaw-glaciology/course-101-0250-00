aaf2e449f2e44f1ea01cb59a228ffa95d1714a21
Mauro Werder <mauro3@runbox.com>
2021-09-09 22:17:19 +0200

    started on lecture 1 slides

---
+++ slide-notebooks/l0-admin.jl
@@ -0,0 +1,23 @@
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+# # The small print of "Solving partial differential equations in parallel on GPUs"
+#
+# ## The Team
+#
+# - Ludovic Räss -- the good
+# - Samuel Omlin -- the bad
+# - Mauro Werder -- the ugly
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+# ## Grading
+#
+#
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+# ## Computing
+#
+# Bring your laptop to all lectures!
+#
+# - Who has access to an Nvidia GPU?
+# - What operating system are you on?
+#
+# JupyterHub and direct access to one of our compute servers.
