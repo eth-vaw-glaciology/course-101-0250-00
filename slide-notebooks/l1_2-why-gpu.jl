86d6ce137aa2f91b97852655d69ea02a98fb16ec
Ludovic Räss <ludovic.rass@gmail.com>
2021-09-10 10:55:05 +0200

    WIP on why gpus.

--- slide-notebooks/l1_2-why-gpu.jl
+++ slide-notebooks/l1_2-why-gpu.jl
@@ -1,8 +1,15 @@
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-# # Lecture 1: Why solve PDEs on a GPU? & The tools to do it
-#
-# ## Why solve PDEs on a GPU?
-# An intro by Ludovic about GPU computing:
-# - why we do it
-# - why it is cool
-# - examples from his research
+md"""
+# _Lecture 1_
+# Why solve PDEs on GPUs? & The tools to do it
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+## Why solve PDEs on a GPU?
+An intro about GPU computing:
+- why we do it
+- why it is cool
+- examples from current research
+"""
