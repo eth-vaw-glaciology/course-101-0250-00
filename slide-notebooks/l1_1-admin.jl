53a93a7b40705c6515a0a6dec170db46387ff585
Ludovic Räss <ludovic.rass@gmail.com>
2021-09-10 08:55:46 +0200

    Use md-strings.

--- slide-notebooks/l1_1-admin.jl
+++ slide-notebooks/l1_1-admin.jl
@@ -1,23 +1,30 @@
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-# # The small print of "Solving partial differential equations in parallel on GPUs"
-#
-# ## The Team
-#
-# - Ludovic Räss -- the good
-# - Samuel Omlin -- the bad
-# - Mauro Werder -- the ugly
+md"""
+# The small print of "Solving partial differential equations in parallel on GPUs"
 
-#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-# ## Grading
-#
-#
+## The Team
 
+- Ludovic Räss -- the good
+- Samuel Omlin -- the bad
+- Mauro Werder -- the ugly
+"""
+#src #########################################################################
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-# ## Computing
-#
-# Bring your laptop to all lectures!
-#
-# - Who has access to an Nvidia GPU?
-# - What operating system are you on?
-#
-# JupyterHub and direct access to one of our compute servers.
+md"""
+## Grading
+
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+## Computing
+
+Bring your laptop to all lectures!
+
+- Who has access to an Nvidia GPU?
+- What operating system are you on?
+
+JupyterHub and direct access to one of our compute servers.
+"""
+
