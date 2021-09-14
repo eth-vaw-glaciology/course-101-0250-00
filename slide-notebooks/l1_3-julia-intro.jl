709077052c28d8efc6d628fbbd8d23b7234376d5
Mauro Werder <mauro3@runbox.com>
2021-09-14 23:03:47 +0200

    some more prog.  Working on a pre-proc function which allows to add solutions to the notebooks.

--- slide-notebooks/l1_3-julia-intro.jl
+++ slide-notebooks/l1_3-julia-intro.jl
@@ -1,3 +1,5 @@
+using Markdown #src
+
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 md"""
 # Tool for the job: introduction to Julia
@@ -72,6 +74,8 @@ tspan = (0.0, 50.0)
 x₀ = [2.0, 0.0, 0.0]
 sol = solve(ODEProblem(lorenz, x₀, tspan), Tsit5())
 
+# Yes, this takes some time... Julia is Just-Ahead-of-Time compiled.  I.e. Julia is compiling.
+
 #src #########################################################################
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 md"""
@@ -83,7 +87,7 @@ plot(sol, vars=(1,2,3)) # plot Lorenz attractor
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 md"""
 ### Julia in brief
-Julia 1.0 released 2018
+Julia 1.0 released 2018, now at version 1.6.2
 
 Features:
 - general purpose language with a focus on technical computing
@@ -161,14 +165,340 @@ md"""
 ###  Let's get our hands dirty!
 
 We will now look at
-- variable
+- variables and types
 - control flow
 - functions
-- modules and types
+- modules and packages
 
-The documentation of Julia is good and can be found at https://docs.julialang.org.
+The documentation of Julia is good and can be found at https://docs.julialang.org; although for learning it might be a bit terse...
+
+There are also tutorials, e.g. TODO
 """
 
+# Furthermore, documentation can be gotten with `?xyz`
+?cos
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+### Variables and assignments
+https://docs.julialang.org/en/v1/manual/variables/
+"""
+
+a = 4
+b = "a string"
+c = b # now b and c bind to the same value
+
+md"""
+Conventions:
+- variables are (usually) lowercase, words can be separated by `_`
+- function names are lowercase
+- modules and types are in CamelCase
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+### Variables: Unicode
+From https://docs.julialang.org/en/v1/manual/variables/:
+
+Unicode names (in UTF-8 encoding) are allowed:
+
+```jldoctest
+julia> δ = 0.00001
+1.0e-5
+
+julia> 안녕하세요 = "Hello"
+"Hello"
+```
+
+In the Julia REPL and several other Julia editing environments, you can type many Unicode math
+symbols by typing the backslashed LaTeX symbol name followed by tab. For example, the variable
+name `δ` can be entered by typing `\delta`-*tab*, or even `α̂⁽²⁾` by `\alpha`-*tab*-`\hat`-
+*tab*-`\^(2)`-*tab*. (If you find a symbol somewhere, e.g. in someone else's code,
+that you don't know how to type, the REPL help will tell you: just type `?` and
+then paste the symbol.)
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+### Basic datatypes
+- numbers (Ints, Floats, Complex, etc.)
+- strings
+- tuples
+- arrays
+- dictionaries
+"""
+
+1     # 64 bit integer (or 32 bit if on a 32-bit OS)
+1.5   # Float64
+1//2  # Rational
+#-
+typeof(1.5)
+#-
+"a string", (1, 3.5) # and tuple
+#-
+[1, 2, 3,] # array of eltype Int
+#-
+Dict(a=>1, b=>cos)
+
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+### Array exercises
+
+We will use arrays extensively in this course.
+
+Datatypes belonging to AbstactArrays:
+- `Array` (with aliases `Vector`, `Matrix`)
+- `Range`
+- GPU arrays, static arrays, etc
+"""
+
+# Task: assign two vectors to `a`, and `b` and the concatenate them using `;`:
+
+a = [2, 3]
+## b = ...
+## [ ; ]
+#sol b = [4, 5]
+#sol [a ; b]
+
+# Add new elements to the end of Vector `b` (hint look up the documentation for `push!`)
+
+##
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercises
+
+Concatenate a Range, say `1:10`, with a Vector, say [4,5]:
+"""
+
+## [  ;  ]
+
+
+md"""
+Make a random array of size (3,3).  Look up `?rand`.  Assign it to `a`
+"""
+
+##
+
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercise: indexing
+
+Access element `[1,2]` and `[2,1]` of Matrix `a` (hint use []):
+"""
+
+## a[ ... ], a[ ... ]
+
+# Put those two values into a vector
+
+##
+
+# Linear vs Cartesian indexing,
+# access the first element:
+
+a[1]
+a[1,1]
+
+# Access the last element (look up `?end`) both with linear and Cartesian indices
+
+# a[...]
+# a[..., ...]
+
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercise: indexing by ranges
+
+Access the last row of `a` (hint use `1:end`)
+"""
+
+## a[... , ...]
+
+# Access a 2x2 sub-matrix
+## a[ ]
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercises: variable bindings and views
+
+What do you make of
+"""
+
+a = [1 4; 3 4] # note, this is another way to define a Matrix
+c = a
+a[1, 2] = 99
+@assert c[1,2] == a[1,2]
+@assert b[1] != a[1,2]
+
+# Type your answer here (to start editing, double click into this cell.  When done shift+enter):
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercises: variable bindings and views
+
+An assignment _binds_ the same array to both variables
+"""
+c = a
+c[1] = 8
+@assert a[1]==8 # as c and a are the same thing
+@assert a===c  # note the triple `=`
+
+md"""
+Views vs copies:
+
+In Julia indexing with ranges will create a new array with copies of
+the original's entries. Consider
+"""
+a = rand(3,4)
+b = a[1:3, 1:2]
+b[1] = 99
+@assert a[1] != b[1]
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercises: variable bindings and views
+
+But the memory footprint will be large if we work with large arrays and take sub-arrays of them.
+
+Views to the rescue
+"""
+a = rand(3,4)
+b = @view a[1:3, 1:2]
+b[1] = 99
+
+# check whether the change in `b` is reflected in `a`:
+
+## @assert ...
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Detour: types
+
+All values have types as we saw above.  Arrays store in their type what type the elements can be.
+
+> Arrays which have concrete element-types are more performant!
+"""
+typeof([1, 2]), typeof([1.0, 2.0])
+
+# Aside, they also store their dimension in the second parameter.
+#
+# The type can be specified at creation
+String["one", "two"]
+
+# Create an array taking `Int` with no elements
+
+##
+
+#-
+# Make an array of type `Any` (which can store any value).  Push a value of type
+# Int and one of type String to it.
+
+##
+
+#-
+# Try to assgin 1.5 to the first element of an array of type Array{Int,1}
+
+##
+
+
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercises
+
+Create a uninitialised Matrix of size (3,3) and assign it to `a`.
+First look up the docs of Array with `?Array`
+"""
+
+#-
+# Test that its size is correct, see `size`
+
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Array exercises: ALL DONE
+
+The rest will be learing-by-doing
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+## Control flow
+
+Julia provides a variety of [control flow constructs](https://docs.julialang.org/en/v1/manual/control-flow/), of which we look at:
+
+  * [Conditional Evaluation](https://docs.julialang.org/en/v1/manual/control-flow/#man-conditional-evaluation): `if`-`elseif`-`else` and `?:` (ternary operator).
+  * [Short-Circuit Evaluation](https://docs.julialang.org/en/v1/manual/control-flow/#Short-Circuit-Evaluation): logical operators `&&` (“and”) and `||` (“or”), and also chained comparisons.
+  * [Repeated Evaluation: Loops](https://docs.julialang.org/en/v1/manual/control-flow/#man-loops): `while` and `for`.
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+### Conditional evaluation
+
+Read the first paragraph of
+https://docs.julialang.org/en/v1/manual/control-flow/#man-conditional-evaluation)
+(up to "and no further condition expressions or blocks are evaluated.")
+"""
+
+#-
+md"""
+Write a test which looks at the start of the string in variable `a`
+(?startswith) and sets `b` accodingly.  If the start is
+- "Wh" then set `b = "Likely a question"`
+- "The " then set `b = "A noun"`
+- otherwise set `b = "no idea"`
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+md"""
+### Conditional evaluation: the "ternary operator" `?`
+
+Look up the docs for `?` (i.e. evaluate `??`)
+
+Re-write using `?`
+```
+if a > 5
+    "really big"
+else
+    "not so big"
+end
+```
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+### Short cirquit operators `&&` and `||`
+
+Read https://docs.julialang.org/en/v1/manual/control-flow/#Short-Circuit-Evaluation
+
+Explain what this does
+
+```
+a < 0 && error("Not valid input for `a`")
+```
+"""
+
+# Your answer here:
 
 
 
