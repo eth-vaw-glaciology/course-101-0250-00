400e83ec2f4ce58ab905985e320669f654a31d0f
Ludovic Raess <ludovic.rass@gmail.com>
2021-10-03 09:41:33 +0200

    Add template for lecture 3 repl pkg content.

---
+++ slide-notebooks/l3_2-repl-pkg.jl
@@ -0,0 +1,16 @@
+#src # This is needed to make this run as normal Julia file
+using Markdown #src
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+#nb # _Lecture 3_
+md"""
+# Julia's REPL and `Pkg` manager
+"""
+
+#src INFO - use only level 2 ## and 3 ### titles
+
+#src ######################################################################### 
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+content
+"""
