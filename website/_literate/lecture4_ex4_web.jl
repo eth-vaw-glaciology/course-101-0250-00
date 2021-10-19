a1d787e3ca94a60ec46643edf30073a030fc1699
Ludovic Raess <ludovic.rass@gmail.com>
2021-10-19 17:27:50 +0200

    Update tasks lecture 4.

--- website/_literate/lecture4_ex4_web.jl
+++ website/_literate/lecture4_ex4_web.jl
@@ -13,7 +13,7 @@ The goal of this exercise is to:
 md"""
 In this exercise you will transform the fast implicit nonlinear diffusion 1D solver from Exercise 3 to 2D.
 
-To get started, save a copy of the `diffusion_nl_1D_steady_2.jl` script from Exercise 3 - Task 2, and name it `diffusion_nl_2D_steady_2.jl`.
+To get started, save a copy of the `diffusion_nl_1D_steady_2.jl` script from [Exercise 3 - Task 2](#task_2__3), and name it `diffusion_nl_2D_steady_2.jl`.
 """
 
 md"""
@@ -33,9 +33,9 @@ Use these "radius" functions to set values of `D0` to 1.5 when smaller then 1.0.
 As boundary conditions, set `C=0.5` at $x=dx/2$ and `C=0.1` at $x=Lx-dx/2$. Implement a "no-flux" boundary condition ($∆C$ vanishes in the direction orthogonal to the boundary) at $y=dy/2$ and $y=Ly-dy/2$.
 """
 
-#nb # > 💡 hint: Take care to adapt the iterative time step condition for 2D diffusion and think about how to modify the `maxloc` function for 2D purposes, taking the local maximum amongst all 8 neighbours for each grid point.`
-#md # \note{Take care to adapt the iterative time step condition for 2D diffusion and think about how to modify the `maxloc` function for 2D purposes, taking the local maximum amongst all 8 neighbours for each grid point.}
+#nb # > 💡 hint: Take care to adapt the iterative time step condition for 2D diffusion and think about how to modify the `maxloc()` function for 2D purposes, taking the local maximum amongst all 8 neighbours for each grid point.`
+#md # \note{Take care to adapt the iterative time step condition for 2D diffusion and think about how to modify the `maxloc()` function for 2D purposes, taking the local maximum amongst all 8 neighbours for each grid point.}
 
 md"""
-Report graphically the distribution of concentration `C` as function of `x` and `y` using a heatmap plot, adding axes labels and title reporting time, iteration count and current error.
+Report graphically the distribution of concentration `C` as function of `x` and `y` using a heatmap plot, adding axes labels and title reporting time, iteration count and current error. Use `mean(dt)` to cumulate numerical time *(note that this measure is no longer relevant)*.
 """
