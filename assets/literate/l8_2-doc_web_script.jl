85041cd717a2ff5a5996f995ace0ab9d57a9ffaf
mauro3 <mauro3@runbox.com>
2021-11-09 16:20:58 +0000

    Deploying to gh-pages from  @ b5e9d6093d0995f9a88e76ef439445540fba5c35 🚀

--- assets/literate/l8_2-doc_web_script.jl
+++ assets/literate/l8_2-doc_web_script.jl
@@ -1,2 +1,29 @@
 # This file was generated, do not modify it.
 
+"""
+    transform(r, θ) = (r*cos(θ), r*sin(θ))
+
+Transform polar to cartesian coordinates.
+"""
+transform(r, θ) = (r*cos(θ), r*sin(θ))
+
+"Typical size of beer crate"
+const BEERBOX = 12
+
+?BEERBOX
+
+"""
+    transform(r, θ) = (r*cos(θ), r*sin(θ))
+
+Transform polar to cartesian coordinates.
+
+# Example
+```jldoctest
+julia> transform(4.5, pi/5)
+(3.6405764746872635, 2.6450336353161292)
+```
+"""
+transform(r, θ) = (r*cos(θ), r*sin(θ))
+
+?transform
+
