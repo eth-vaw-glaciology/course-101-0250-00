2d3cfc1ab88b1a7e5cea3bbd7cb70aa89fce3042
Ludovic Raess <ludovic.rass@gmail.com>
2021-11-15 22:52:41 +0100

    Add directions.

--- website/_literate/lecture9_ex1_web.jl
+++ website/_literate/lecture9_ex1_web.jl
@@ -6,16 +6,19 @@ md"""
 
 md"""
 The goal of this exercise is to:
+- ensure you can run a Julia notebook on your octopus node
 - refresh how to establish the peak memory throughput of your GPU
 """
 
 md"""
 ### Getting started
 
-👉 Download the introduction notebook *Benchmarking memory copy and establishing peak memory access performance* ([`l6_1-gpu-memcopy.ipynb`](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/slide-notebooks/notebooks/l6_1-gpu-memcopy.ipynb))
-
-Then run the notebook to establish the performance baseline *(you should have a copy of it in your `lecture09` folder on octopus)*.
-
+👉 Make sure:
+1. You are logged-in to your node on octopus ([how-to](/software_install/#login_to_your_node))
+2. Head to `/scratch/<username>/lecture09/`
+3. Get your Julia environment ready so that you can [start a Jupyter notebook server](/software_install/#running_a_jupyter_notebook)
+4. Open the introduction notebook *Benchmarking memory copy and establishing peak memory access performance* (`/scratch/<username>/lecture09/l6_1-gpu-memcopy.ipynb`)
+5. Run the notebook to establish the performance baseline *(you should have a copy of it in your `lecture09` folder on octopus)*.
 """
 
 
