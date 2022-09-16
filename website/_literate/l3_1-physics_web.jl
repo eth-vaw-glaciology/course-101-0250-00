b08a7ae7cbc3b05ba7072a02d6745542c909ae28
Ivan Utkin <iutkin@ethz.ch>
2022-09-16 20:09:20 +0200

    Update lecture3

--- website/_literate/l3_1-physics_web.jl
+++ website/_literate/l3_1-physics_web.jl
@@ -87,177 +87,266 @@ md"""
 
 Let's add diffusive properties to the wave equation by simply combining the physics:
 
-$$
-\rho\frac{\partial V_x}{\partial t} = -\frac{\partial Pr}{\partial x}
-$$
-
-$$
-\beta\frac{\partial Pr}{\partial t} + \frac{Pr}{\eta} = -\frac{\partial Vx}{\partial x}
-$$
+\begin{align}
+\rho\frac{\partial V_x}{\partial t}                 &= -\frac{\partial P}{\partial x} \\
+\beta\frac{\partial P}{\partial t} + \frac{P}{\eta} &= -\frac{\partial V_x}{\partial x}
+\end{align}
 """
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-Note the addition of the new term $\frac{Pr}{\eta}$ to the left-hand side of the mass balance equation, which could be interpreted physically as adding the bulk viscosity to the gas.
+Note the addition of the new term $\frac{Pr}{\eta}$ to the left-hand side of the mass balance equation, which could be interpreted physically as accounting for the bulk viscosity of the gas.
 """
 
 #src #########################################################################
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 md"""
-Equvalently, we could add the time derivatives to the diffusion equation
+Equvalently, we could add the time derivative to the diffusion equation
 
-$$
-\rho\frac{\partial q}{\partial t} + \frac{q}{D} = -\frac{\partial C}{\partial x}
-$$
-
-$$
-\frac{\partial C}{\partial t} = -\frac{\partial q}{\partial x}
-$$
+\begin{align}
+\rho\frac{\partial q}{\partial t} + \frac{q}{D} &= -\frac{\partial C}{\partial x} \\
+\frac{\partial C}{\partial t}                   &= -\frac{\partial q}{\partial x}
+\end{align}
 """
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-In that case, the new term would be `\rho\frac{\partial q}{\partial t}`, which could be interpreted physically as adding inertia to the momentum equation for diffusive flux.
+In that case, the new term would be $\rho\frac{\partial q}{\partial t}$, which could be interpreted physically as adding the inertia to the momentum equation for diffusive flux.
 """
 
 #src #########################################################################
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 md"""
-## From 1D to 2D
-
-Let's discuss how to implement the acoustic wave equation (and the diffusion equation from last week's material) in 2D.
+Note that in 1D the both modifications are equivalent up to renaming the variables. The conceptual difference is that in the former case we add new terms to the vector quantity (diffusive flux $q$), and in the latter case we modify the equation governing the evolution of the scalar quantity (pressure $P$). 
 """
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-We want the $x$ and $y$ axis to represent spatial extend, and solve in each grid point for the pressure or the concentration, for the acoustic and diffusion process, respectively.
-"""
-
-#src #########################################################################
-#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-md"""
-But let's first look at the equation, augmenting the Table we just started to fill
-"""
-
-#!nb # |  Physics       |  1D formulation  |  2D formulation  |
-#!nb # | :------------: | :--------------: | :--------------: |
-#!nb # | Diffusion      | $q_x = -D\frac{∂C}{∂x}$            | $q_x = -D\frac{∂C}{∂x}$ |
-#!nb # |                |                                    | $q_y = -D\frac{∂C}{∂y}$ |
-#!nb # |                | $\frac{∂C}{∂t} = -\frac{∂q_x}{∂x}$ | $\frac{∂C}{∂t} = -\left(\frac{∂q_x}{∂x} + \frac{∂q_y}{∂y} \right)$ |
-#!nb # | Acoustic waves | $\frac{∂V_x}{∂t} = -\frac{1}{ρ}~\frac{∂P}{∂x}$ | $\frac{∂V_x}{∂t} = -\frac{1}{ρ}~\frac{∂P}{∂x}$ |
-#!nb # |                |                                                | $\frac{∂V_y}{∂t} = -\frac{1}{ρ}~\frac{∂P}{∂y}$ |
-#!nb # |                | $\frac{∂P}{∂t} = -K~\frac{∂V_x}{∂x}$           | $\frac{∂P}{∂t} = -K~\left(\frac{∂V_x}{∂x} + \frac{∂V_y}{∂y} \right)$ |
-
-
-#nb # |  Physics       |  1D formulation |  2D formulation |
-#nb # | ------------:  | :-------------- | :-------------- |
-#nb # | Diffusion      | $$q_x = -D\frac{∂C}{∂x}$$  $$\frac{∂C}{∂t} = -\frac{∂q_x}{∂x}$$ | $$q_x = -D\frac{∂C}{∂x}$$  $$q_y = -D\frac{∂C}{∂y}$$  $$\frac{∂C}{∂t} = -\left(\frac{∂q_x}{∂x} + \frac{∂q_y}{∂y} \right)$$ |
-#nb # | Acoustic waves | $$\frac{∂V_x}{∂t} = -\frac{1}{ρ}~\frac{∂P}{∂x}$$  $$\frac{∂P}{∂t} = -K~\frac{∂V_x}{∂x}$$ |$$\frac{∂V_x}{∂t} = -\frac{1}{ρ}~\frac{∂P}{∂x}$$  $$\frac{∂V_y}{∂t} = -\frac{1}{ρ}~\frac{∂P}{∂y}$$  $$\frac{∂P}{∂t} = -K~\left(\frac{∂V_x}{∂x} + \frac{∂V_y}{∂y} \right)$$ |
-
-
-#src #########################################################################
-#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-md"""
-For both physics
-- The fluxes which are directional or vector quantities have a new $y$-direction component
-- The balance equation or divergence, now balances the sum of the fluxes from all dimensions
-
+Let's eliminate $V_x$ and $q$ in both systems to get one governing equation for $P$ and $C$, respectively:
 """
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-Let's get started first with the diffusion equation, then the wave equation (as homework).
+\begin{align}
+\beta\frac{\partial^2 P}{\partial t^2} + \frac{1}{\eta}\frac{\partial P}{\partial t} &= \frac{1}{\rho}\frac{\partial^2 P}{\partial x^2} \\
+\rho\frac{\partial^2 C}{\partial t^2} + \frac{1}{D}\frac{\partial C}{\partial t}     &= \frac{\partial^2 C}{\partial x^2}
+\end{align}
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+We refer to such equations as the _damped wave equations_. They combine wave propagation with diffusion, which manifests as wave attenuation, or decay. The damped wave equation is a hyperbolic PDE.
 """
 
 #src #########################################################################
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 md"""
-### To dos:
-- Add $y$-direction physics and numerics
-- Update time step definition
-- Update initial Gaussian condition
-- Initialise all new arrays
-- Update physics calculations in the time loop
-- Update plotting
+## Implementing the damped wave equation
+
+In the following, we'll use the damped wave equation for concentration $C$ obtained by augmenting the diffusion equation with density $\rho$.
 """
 
-#src #########################################################################
-#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-#### $y$-direction physics and numerics
+Starting from the existing code implementing time-dependent diffusion, let's add the intertial term $\rho\frac{\partial q}{\partial t}$.
+"""
 
-You can make multi-statement lines for scalars:
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+First step is to add the new physical parameter $\rho$ to the `# physics` section:
 
 ```julia
-Lx, Ly = 10.0, 10.0
+# physics
+...
+ρ   = 20.0
 ```
 """
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-#### Time step definition
-
-Take now the most restrictive condition, e.g.:
+And to change the initial conditions to have more interesting time evolution:
 
 ```julia
-dt = min(dx, dy)/...
+# array initialisation
+C    = @. exp(-(xc-lx/4)^2); C_i = copy(C); C[1] = 1
 ```
-"""
-
-#src #########################################################################
-#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-md"""
-#### Initialise arrays
-
-Recall that we are using conservative finite-differences and thus a _staggered grid_.
-
-For 2D grids, we will have to handle scalar quantity and two fluxes as depicted below, taking care about correct staggering:
-
-![staggered-grid](../assets/literate_figures/stagg_2D.png)
-"""
-
-#src #########################################################################
-#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
-md"""
-#### 2D plotting
-
-You can use `heatmap()` function from `Plots.jl`, to plot e.g. `C` as function of the spatial coordinates `xc` and `yc`:
-
-```julia
-heatmap(xc, yc, C')
-```
-_note the transpose `'`_
-
-Use `display()` to force the display of the plot, e.g., in the time loop every `nout`.
 """
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-More advanced implementation, one can define the plotting options and apply them in the `heatmap()` call:
+Then we modify the time loop to incorporate the new physics:
 
 ```julia
-opts = (aspect_ratio=1, xlims=(xc[1], xc[end]), ylims=(yc[1], yc[end]), clims=(0.0, 1.0), c=:davos, xlabel="Lx", ylabel="Ly", title="time = $(round(it*dt, sigdigits=3))")
-display(heatmap(xc, yc, C'; opts...))
+for it = 1:nt
+    #qx         .-= ...
+    C[2:end-1] .-= dt.*diff(qx)./dx
+    ...
+end
+```
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+👉 Your turn. Try to add the intertial term.
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+> Hint: There are two ways of adding the intertial term into the update rule.
+> - We could either take the known flux `q` in `q/dc` from the previous time step (explicit time integration), or the unknown flux from the next time step (implicit time integration).
+> - Could we treat the flux implicitly without having to solve the linear system?
+> - What are the benefits of the implicit time integration compared to the explicit one?
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+If the implementation is correct, we should see this:
+"""
+
+#md # ~~~
+# <center>
+#   <video width="80%" autoplay loop controls src="../assets/literate_figures/damped_diffusion_1D.mp4"/>
+# </center>
+#md # ~~~
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+The waves decay, now there is a steady state! 🎉 The time it takes to converge, however, doesn't seem to improve...
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+Now we solve the hyperbolic PDE, and with the implicit flux term treatment, the time step should be now proportional to the grid spacing `dx` instead of `dx^2`. Looking at the damped wave equation for $C$, and recalling the stability condition for wave propagation, we modify the time step, reduce the total number of time steps, and increase the frequency of plotting calls:
+
+```julia
+# numerics
+...
+nvis = 5
+# derived numerics
+...
+dt   = dx/sqrt(1/ρ)
+nt   = 5nx
 ```
 """
 
 #src #########################################################################
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
 md"""
-That's how the 2D diffusion looks like:
+Re-run the simulation and see the results:
+"""
 
-![diffusion](../assets/literate_figures/diffusion_2D_1.gif)
+#md # ~~~
+# <center>
+#   <video width="80%" autoplay loop controls src="../assets/literate_figures/damped_diffusion_better_1D.mp4"/>
+# </center>
+#md # ~~~
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+Now, this is much better! However, we introduced the new parameter $\rho$, does the solution depend on the value of $\rho$?
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+## Problem of finding the iteration parameters
+
+👉 Try changing the new parameter `ρ`, increase and decrease it. What happens to the solution?
 """
 
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
 md"""
-Let's get started with 2D.
-
-**It's time to launch Julia on your computer** 🚀
-
-👉 [Download the `diffusion_1D.jl` script](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/scripts/) to get you started
+We noticed that depending on the value of the parameter `ρ`, the convergence to steady-state can be faster or slower. If `ρ` is too small, the process becomes diffusion-dominated, and we're back to the non-accelerated version. If `ρ` is too large, waves decay too slow.
 """
 
-#sol #md # 👉 [Download the `diffusion_2D.jl` script](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/scripts/).
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+If the parameter `ρ` has optimal value, the convergence to steady-state could be achieved in the number of time steps proportional to the number of grid points `nx` and not `nx^2` as for the parabolic PDE.
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+## Historical perspective
+
+The idea of accelerating the convergence by increasing the order of PDE dates back to the work by [Frankel (1950)](https://doi.org/10.2307/2002770) where he studied the convergence rates of different iterative methods. Frankel noted the analogy between the iteration process and transient physics. In his work, the accelerated method was called the _second-order Richardson method_
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+In this course, we call this and any method that builds upon the analogy to transient physics... the _pseudo-transient_ method.
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+Using this analogy proves useful when studying multi-physics and nonlinear processes. The pseudo-transient method isn't restricted to solving the Poisson problems, but can be applied to a wide range of problems that are modeled with PDEs.
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+## Pseudo-transient method
+
+In a pseudo-transient method, we are interested only in a steady-state distributions of the unknown field variables such as concentration, temperature, etc.
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+We consider time steps as iterations in a numerical method. Therefore, we replace the time $t$ in the equations with _pseudo-time_ $\tau$, and a time step `it` with iteration counter `iter`. When a pseudo-transient method converges, all the pseudo-time derivatives $\partial/\partial\tau$, $\partial^2/\partial\tau^2$ etc., vanish.
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+We should be careful when introducing the new pseudo-physical terms into the governing equations. We need to make sure that when iterations converge, i.e., if the pseudo-time derivatives are set to 0, the system of equations is identical to the original steady-state formulation.
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+For example, consider the damped acoustic problem that we introduced in the beggining:
+
+\begin{align}
+\rho\frac{\partial V_x}{\partial\tau}                   &= -\frac{\partial P}{\partial x} \\
+\beta\frac{\partial P}{\partial\tau} + \frac{P}{\eta} &= -\frac{\partial V_x}{\partial x}
+\end{align}
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+At the steady-state, the second equation reads:
+
+$$
+\frac{P}{\eta} = -\frac{\partial V_x}{\partial x}
+$$
+
+The velocity divergence is proportional to the pressure. If we wanted to solve the incompressible problem (i.e. the velocty divergence = 0), and were interested in the velocity distribution, this approach would lead to incorrect results. If we only want to solve the Laplace problem $\partial^2 P/\partial x^2 = 0$, we could consider $V_x$ purely as a numerical variable.
+"""
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+md"""
+In other words: only add those new terms to the governing equations that vanish when the iterations converge!
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+## Dispersion analysis of the PDEs
+
+We don't want to guess the optimal parameter values for every problem.
+For linear problems with constant coefficients, there is a way to get an exact optimal value for any combination of phyisics and boundary conditions. Analytics is hard, so we'll consider only the simplest elliptic problem with constant values at boundaries:
+"""
+
+#src #########################################################################
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+md"""
+Let's try this value:
+
+```julia
+ρ    = (lx/(dc*2π))^2
+```
+"""
+
 
 
