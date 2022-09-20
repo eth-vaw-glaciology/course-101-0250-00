e0018e768d2e35e53ffcaeef54290cab1936f1d2
Ludovic Raess <ludovic.rass@gmail.com>
2022-09-20 09:19:02 +0200

    Add content to be deployed on JupyterHub

---
+++ JupyterHub/lecture-1/lecture1_ex3.jl
@@ -0,0 +1,53 @@
+md"""
+## Exercise 3 - **Volcanic bomb**
+"""
+
+#md # 👉 [Download the notebook to get started with this exercise!](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/exercise-notebooks/notebooks/lecture1_ex3.ipynb)
+#md #
+
+#md # \warn{Write a monolithic Julia script to solve this exercise in a Jupyter notebook and hand it in on Moodle ([more](/homework)).}
+
+md"""
+The goal of this exercise is to consolidate:
+- code structure `# Physics, # Numerics, # Time loop, # Visualisation`
+- array initialisation
+- update rule
+- `if` condition
+"""
+
+md"""
+You will now simulate the trajectory of a [volcanic bomb](https://en.wikipedia.org/wiki/Volcanic_bomb) that got ejected during a volcanic eruption. The ejection speed is given by the horizontal and vertical velocity components
+$$
+V_x = \frac{\partial x}{\partial t}\\[10pt]
+V_y = \frac{\partial y}{\partial t}
+$$
+
+Once ejected, the volcanic bomb is subject to gravity acceleration $g$. Air friction will be neglected. Acceleration being defined as the change of velocity over time, we obtain the following update rule:
+$$
+\frac{\partial V_y}{\partial t}=-g
+$$
+
+These equations define a mathematical model describing the kinematics of the volcanic bomb. You may remember from your studies how to solve those equation analytically; however we'll here focus on a numerical solution using a similar approach as for the previous exercises. The $x$ and $y$ location of the bomb as function of time can be obtained based on updating previous values using the definition of velocity:
+$$
+x_{t+\Delta t} = x_{t} + V_x    \Delta t~,\\[5pt]
+y_{t+\Delta t} = y_{t} + V_y(t) \Delta t~.
+$$
+And because of gravity acceleration, the $V_y$ velocity evolution can be obtained according to
+$$
+V_{y,t+\Delta t} = V_{y,t} - g \Delta t~.
+$$
+
+The 3 equations above represent the discretised form of the 3 first equations and should be used to solve the problem numerically. The initial position of the volcanic bomb $(x_0, y_0)=(0,480)$ m. The magnitude of the ejection speed is of 120 m/s and the angle $\alpha = 60°$. The simulation stops when the volcanic bomb touches the ground ($y=0$).
+"""
+
+#nb # > 💡 hint:
+#nb # > - Modify the code from exercise 2 to, in addition, account for the change of `Vy` with time
+#nb # > - Use e.g. a `break` statement to exit the loop once the bomb hits the ground
+#md # \note{- Modify the code from exercise 2 to, in addition, account for the change of `Vy` with time
+#md # - Use e.g. a `break` statement to exit the loop once the bomb hits the ground}
+
+md"""
+### Question 1
+
+Report the height of the volcanic bomb at position $x=900$ m away from origin.
+"""
---
+++ JupyterHub/lecture-1/lecture1_ex5.jl
@@ -0,0 +1,48 @@
+md"""
+## Exercise 5 _(optional)_ - **Many volcanic bombs**
+"""
+
+#md # 👉 [Download the notebook to get started with this exercise!](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/exercise-notebooks/notebooks/lecture1_ex5.ipynb)
+
+#md # \warn{Write a monolithic Julia script to solve this exercise in a Jupyter notebook and hand it in on Moodle ([more](/homework)).}
+
+md"""
+The goal of this exercise is to consolidate:
+- vectorisation and element-wise operations using `.`
+- random numbers
+- array initialisation
+- conditional statement
+"""
+
+md"""
+The goal is to extend the volcanic bomb calculator (exercise 3) to handle `nb` bombs.
+
+To do so, start from the script you wrote to predict the trajectory of a single volcanic bomb and extend it to handle `nb` bombs.
+
+Declare a new variable for the number of volcanic bombs
+"""
+nb = 5 # number of volcanic bombs
+
+md"""
+Then, replace the vertical angle of ejection α to randomly vary between 60° and 120° with respect to the horizon for each bomb. Keep the magnitude of the ejection velocity as before, i.e. $V=120$ m/s.
+"""
+
+#nb # > 💡 hint: Use the `randn()` function to generate random numbers normally distributed.
+#md # \note{Use the `randn()` function to generate random numbers normally distributed.}
+
+md"""
+All bombs have the same initial location $(x=0, y=480)$ as before.
+
+Implement the necessary modifications in the time loop in order to update the position of all volcanic bombs correctly.
+
+Ensure the bombs stop their motion once they hit the ground (at position $y=0$).
+
+### Question 1
+
+Report the total time it takes for the last, out of 5, volcanic bombs to hit the ground and provide a figure that visualise the bombs' overall trajectories.
+
+### Question 2
+
+Repeat the exercise 1 but vectorise all your code, i.e. make use of broadcasting capabilities in Julia (using `.` operator) to only have a single loop for advancing in time.
+
+"""
