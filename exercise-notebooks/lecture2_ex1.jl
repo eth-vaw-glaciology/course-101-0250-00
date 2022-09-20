5d0480a00be3d4e62f6608c2c24725ff4c891ba8
Ludovic Raess <ludovic.rass@gmail.com>
2022-09-20 11:35:14 +0200

    Update ex lecture 2

--- exercise-notebooks/lecture2_ex1.jl
+++ exercise-notebooks/lecture2_ex1.jl
@@ -7,7 +7,7 @@ md"""
 #md # \warn{Write a monolithic Julia script to solve this exercise in a Jupyter notebook and hand it in on Moodle ([more](/homework)).}
 
 md"""
-The goal of this exercise is to consolidate:
+The goal of this exercise is to couple physical processes implementing:
 - 1D advection-diffusion
 - Non-dimensional numbers
 """
@@ -19,21 +19,21 @@ From what you learned in class, write an advection-diffusion code having followi
 
 ```
 # Physics
-Lx    = 10.0  # domain length
-D     = 0.4   # diffusion coefficient
-vx    = 1.0   # advection velocity
-ttot  = 2.0   # total simulation time
+lx   = 20.0  # domain length
+dc   = 0.1   # diffusion coefficient
+vx   = 1.0   # advection velocity
+ttot = 20.0  # total simulation time
 ```
 
-Discretise your domain in 128 finite-difference cells such that the first cell centre is located at `dx/2` and the last cell centre at `Lx-dx/2`. Use following explicit time step limiters:
+Discretise your domain in 200 finite-difference cells such that the first cell centre is located at `dx/2` and the last cell centre at `lx-dx/2`. Use following explicit time step limiters:
 
 ```julia
-dtd   = dx^2/D/2.6
-dta   = dx/vx
-dt    = min(dtd, dta)
+dta  = dx/abs(vx)
+dtd  = dx^2/dc/2
+dt   = min(dtd, dta)
 ```
 
-As initial condition, define a Gaussian profile of concentration `C` of amplitude and standard deviation equal to 1, located at `0.3*Lx`.
+As initial condition, define a Gaussian profile of concentration `C` of amplitude and standard deviation equal to 1, located at `lx/4`.
 
 Keep the concentration at the boundaries at `C=0`.
 """
@@ -48,5 +48,5 @@ Report the initial and final distribution of concentration on a figure with axis
 
 ### Question 2
 
-Repeat the exercise but introduce the non-dimensional [Péclet number](https://en.wikipedia.org/wiki/Péclet_number) $Pe = L~vx/D$ as physical quantity defining the diffusion coefficient D as a `# Derived physics` quantity. Confirm the if $Pe >> 1$ the diffusion happens in a much longer time compared to the advection, and the opposite for $Pe << 1$.
+Repeat the exercise but introduce the non-dimensional [Péclet number](https://en.wikipedia.org/wiki/Péclet_number) $Pe = lx~vx/dc$ as physical quantity defining the diffusion coefficient `dc` as a `# Derived physics` quantity. Confirm the if $Pe >> 100$ the diffusion happens in a much longer time compared to the advection, and the opposite for $Pe << 100$.
 """
--- exercise-notebooks/lecture2_ex2.jl
+++ exercise-notebooks/lecture2_ex2.jl
@@ -7,34 +7,38 @@ md"""
 #md # \warn{Write a monolithic Julia script to solve this exercise in a Jupyter notebook and hand it in on Moodle ([more](/homework)).}
 
 md"""
-The goal of this exercise is to consolidate:
+The goal of this exercise is to couple physical processes implementing:
 - 1D reaction-diffusion
 - Non-dimensional numbers
 """
 
 md"""
-The goal of this exercise is to combine reaction and diffusion processes acting on the concentration of a quantity $C$.
+The goal of this exercise is to combine reaction and diffusion processes acting on the concentration of a quantity $C$. The reaction equation is an ODE, as the evolution of the quantity $C$ does not depend on the neighbouring values (or spatial gradients). Consider the following reaction ODE
+
+$$ \frac{∂C}{∂t} = -\frac{(C-C_\mathrm{eq})}{ξ}~,$$
+
+where $C_\mathrm{eq}$ is the equilibrium concentration and $ξ$ the reaction rate.
 
 From what you learned in class, write an reaction-diffusion code having following physical input parameters:
 
 ```
 # Physics
-Lx    = 10.0  # domain length
-D     = 0.4   # diffusion coefficient
-ξ     = 10.0  # reaction rate
-Ceq   = 0.4   # equilibrium concentration
-ttot  = 2.0   # total simulation time
+lx   = 20.0  # domain length
+dc   = 0.1   # diffusion coefficient
+ξ    = 10.0  # reaction rate
+Ceq  = 0.4   # equilibrium concentration
+ttot = 20.0  # total simulation time
 ```
 
-Discretise your domain in 128 finite-difference cells such that the first cell centre is located at `dx/2` and the last cell centre at `Lx-dx/2`. Use following explicit time step limiters:
+Discretise your domain in 200 finite-difference cells such that the first cell centre is located at `dx/2` and the last cell centre at `lx-dx/2`. Use following explicit time step limiters:
 
 ```julia
-dt    = dx^2/D/2.2
+dt   = dx^2/dc/2
 ```
 
-As initial condition, define a Gaussian profile of concentration `C` of amplitude and standard deviation equal to 1, located at `0.3*Lx`.
+As initial condition, define a Gaussian profile of concentration `C` of amplitude and standard deviation equal to 1, located at `lx/4`.
 
-Keep the concentration at the boundaries at `C=0`.
+Only update the concentration at the boundary points with the reaction process.
 """
 
 #nb # > 💡 hint: Don't forget to initialise (pre-allocate) all arrays (vectors) needed in the calculations.
@@ -47,5 +51,5 @@ Report the initial and final distribution of concentration on a figure with axis
 
 ### Question 2
 
-Repeat the exercise but introduce the non-dimensional [Damköhler number](https://en.wikipedia.org/wiki/Damköhler_numbers) $Da = L^2/D/ξ$ as physical quantity defining the diffusion coefficient D as a `# Derived physics` quantity. Confirm the if $Da < 0.1$ most of the mass diffuses away from $C_{eq}$, and the opposite holds for $Da > 10$.
+Repeat the exercise but introduce the non-dimensional [Damköhler number](https://en.wikipedia.org/wiki/Damköhler_numbers) $Da = lx^2/dc/ξ$ as physical quantity defining the diffusion coefficient `dc` as a `# Derived physics` quantity. Confirm the if $Da < 0.1$ most of the mass diffuses away from $C_{eq}$, and the opposite holds for $Da > 1000$.
 """
