507d5e639e3dfe2b01d3042a8d0b6a11ed6d1c99
Ludovic Raess <ludovic.rass@gmail.com>
2022-09-27 16:37:20 +0200

    Update content

--- exercise-notebooks/deploy_notebooks.jl
+++ exercise-notebooks/deploy_notebooks.jl
@@ -1,6 +1,6 @@
 using Literate
 ## include Literate scripts starting with following letters in the deploy
-incl = "lecture1"
+incl = "lecture2"
 ## Set `sol=true` to produce output with solutions contained and hints stripts. Otherwise the other way around.
 sol = false
 ##
