5e7e15a6f4d096bce229e9fc4c7870b0a2da8007
Ivan Utkin <iutkin@ethz.ch>
2022-10-02 17:09:37 +0200

    Add outlines for exercises

--- exercise-notebooks/lecture3_ex2.jl
+++ exercise-notebooks/lecture3_ex2.jl
@@ -1,28 +1,9 @@
 md"""
-## Exercise 2 - **Acoustic waves in 2D - v2**
+## Exercise 2 - ** Operator-splitting for advection-diffusion**
 """
 
 #md # 👉 See [Logistics](/logistics/#submission) for submission details.
 
 md"""
-The goal of this exercise is to:
-- Implement 2D wave equation
-- Consolidate the finite-difference discretisation
-- Familiarise with visualisation
-"""
-
-md"""
-In this second exercise, you will implement a more concise version of the 2D wave equation.
-
-Starting from the 2D wave equation code from exercise 1, reformulate the physics calculation without the explicit definition of the $q_x, q_y$ terms; only use velocities $V_x, V_y$ and pressure $P$.
-
-### Task 1
-
-Create a new Julia script `acoustic_2D_v2.jl` for this homework. The script should produce a `heatmap()` plot that updates upon time steps, with labelled axes and physical time displayed as title.
-
-Use `nx = 128` and `ny = 129` grid points and the same parameters as for exercise 1.
-
-### Task 2
-
-Create a 3-panels plot that shows the 2D pressure $P$ and the velocity $V_x$ fields, as well as the 1D cross-section of the pressure field at $Ly/2$.
+Operator-splitting for advection-diffusion, we do the implicit diffusion in PT-loop, followed by one step of advection, thus we could use optimal time step for advection
 """
