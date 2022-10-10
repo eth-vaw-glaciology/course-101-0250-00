b19cf7dac0c080f700311933b64282c6454937b2
Ludovic Raess <ludovic.rass@gmail.com>
2022-10-11 00:55:27 +0200

    Update exercises

--- website/_literate/lecture4_ex1_web.jl
+++ website/_literate/lecture4_ex1_web.jl
@@ -6,20 +6,13 @@ md"""
 
 md"""
 The goal of this exercise is to:
-- Implement a 2D nonlinear diffusion equation
-- Consolidate the finite-difference discretisation
+- Implement coupled diffusion equations in 2D
+- Consolidate the implicit updates and dual timestepping
 """
 
 md"""
-In this first exercise, you will port the shallow ice-like 1D nonlinear diffusion code we did in class to a 2D implementation.
+In this first exercise, you will finalise the thermal porous convection discussed and implemented in class.
 
-Starting from the 1D nonlinear diffusion equation we discussed in lecture 4, extend the 1D code to a 2D configuration. Use the same parameters for the $y$-dimension quantities as the one you already have for the $x$-dimension.
-"""
-
-#nb # > 💡 hint: Don't forget to initialise (pre-allocate) all arrays (vectors) needed in the calculations. You will also need to handle the effective diffusivities in both $x$ and $y$ directions to account for correct staggering.
-#md # \note{Don't forget to initialise (pre-allocate) all arrays (vectors) needed in the calculations. You will also need to handle the effective diffusivities in both $x$ and $y$ directions to account for correct staggering.}
-
-md"""
 ### Task 1
 Create a new folder in your GitHub repository for this week's (lecture 4) exercises. In there, create a new Julia script `porous_convection_2D.jl` for this homework. Take 1D steady diffusion script `l3_steady_diffusion_1D.jl` as a basis. Rename variables so that we solve it for the pressure.
 """
@@ -46,7 +39,7 @@ Add new fields for the temperature evolution (advection and diffusion). Add the 
 md"""
 ### Task 5
 
-Add two-way coupling using the Boussinesq approximaiton, i.e., the dependence of density on temperature in the Darcy flux. Produce the animation displaying the temperature evolution and arrows for velocities.
+Add two-way coupling using the Boussinesq approximation, i.e., the dependence of density on temperature in the Darcy flux. Produce the animation displaying the temperature evolution and arrows for velocities.
 
 ```julia
 Vscale = 1/maximum(sqrt.(Vxp.^2 + Vyp.^2)) * dx*(st-1)
