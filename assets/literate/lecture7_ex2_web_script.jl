58d740ddc0d694c3b3834794563672ad44411be2
luraess <61313342+luraess@users.noreply.github.com>
2022-11-01 07:43:57 +0000

    Deploying to gh-pages from  @ 3a69f9a6832dd608c6db75ace449c45f8f67da2a 🚀

--- assets/literate/lecture7_ex2_web_script.jl
+++ assets/literate/lecture7_ex2_web_script.jl
@@ -4,11 +4,17 @@
 lx,ly,lz    = 40.0,20.0,20.0
 αρg         = 1.0
 Ra          = 1000
-λ_ρCp       = 1/Ra*(αρg*k_ηf*ΔT*lz/ϕ) # Ra = αρg*k_ηf*ΔT*ly/λ_ρCp/ϕ
+λ_ρCp       = 1/Ra*(αρg*k_ηf*ΔT*lz/ϕ) # Ra = αρg*k_ηf*ΔT*lz/λ_ρCp/ϕ
 # numerics
 nz          = 63
 ny          = nz
 nx          = 2*(nz+1)-1
 nt          = 500
 cfl         = 1.0/sqrt(3.1)
+
+dt = if it == 1
+    0.1*min(dx,dy,dz)/(αρg*ΔT*k_ηf)
+else
+    min(5.0*min(dx,dy,dz)/(αρg*ΔT*k_ηf),ϕ*min(dx/maximum(abs.(qDx)), dy/maximum(abs.(qDy)), dz/maximum(abs.(qDz)))/3.1)
+end
 
