c784fec074242c32233fed6433683688fa058c03
luraess <61313342+luraess@users.noreply.github.com>
2022-11-15 10:55:32 +0000

    Deploying to gh-pages from  @ 8c6b5b07f81560aaa12c5ee2413b8a760eb896fa 🚀

--- assets/literate/lecture9_ex2_web_script.jl
+++ assets/literate/lecture9_ex2_web_script.jl
@@ -1,2 +1,18 @@
 # This file was generated, do not modify it.
 
+"""
+Some docstring
+"""
+function save_array(Aname,A)
+    fname = string(Aname,".bin")
+    out = open(fname,"w"); write(out,A); close(out)
+end
+
+"""
+Some docstring
+"""
+function load_array(Aname,A)
+    fname = string(Aname,".bin")
+    fid=open(fname,"r"); read!(fid,A); close(fid)
+end
+
