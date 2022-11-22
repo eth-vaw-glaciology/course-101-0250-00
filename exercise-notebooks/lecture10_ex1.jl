17942debd7548f2b4398d2bb9704a0128e43d5e4
Ludovic Raess <ludovic.rass@gmail.com>
2022-11-22 08:31:56 +0100

    fix typo

--- exercise-notebooks/lecture10_ex1.jl
+++ exercise-notebooks/lecture10_ex1.jl
@@ -14,8 +14,8 @@ md"""
 ### Getting started
 
 👉 Make sure:
-1. Log into your [JupyterLab](software_install/#jupyterhub) on Piz Daint
+1. Log into your [JupyterLab](/software_install/#jupyterhub) on Piz Daint
 2. Download the [`l6_1-gpu-memcopy.ipynb`](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/slide-notebooks/notebooks/l6_1-gpu-memcopy.ipynb) notebooks and copy it to your `scratch` space on Piz Daint.
-4. Open the introduction notebook *Benchmarking memory copy and establishing peak memory access performance* [l6_1-gpu-memcopy.ipynb`](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/slide-notebooks/notebooks/l6_1-gpu-memcopy.ipynb)
+4. Open the introduction notebook *Benchmarking memory copy and establishing peak memory access performance* [`l6_1-gpu-memcopy.ipynb`](https://github.com/eth-vaw-glaciology/course-101-0250-00/blob/main/slide-notebooks/notebooks/l6_1-gpu-memcopy.ipynb)
 5. Run the notebook to establish the performance baseline (you will need the value in the next exercises).
 """
