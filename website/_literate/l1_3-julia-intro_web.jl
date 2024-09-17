b4dcca5b6a92e3cd13427796d677a37ebbf84dbd
Ludovic Raess <ludovic.rass@gmail.com>
2024-09-17 14:37:57 +0200

    Upload missing asset and fix size in md (website)

--- website/_literate/l1_3-julia-intro_web.jl
+++ website/_literate/l1_3-julia-intro_web.jl
@@ -37,8 +37,9 @@ md"""
 7. Coq, Brainfuck, ...
 
 ### Survery for you to fill in now (click or scan, 3min):
-[![survey](../assets/literate_figures/l1_survey_qr.png)](https://forms.gle/FRxEV3PU7iKx5q3L8)
 """
+#nb # [![survey](../assets/literate_figures/l1_survey_qr.png)](https://forms.gle/FRxEV3PU7iKx5q3L8)
+#md # ~~~ <img src="../assets/literate_figures/l1_survey_qr.png" alt="survey" style="width:200px;"/> ~~~
 
 #src #########################################################################
 #nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
