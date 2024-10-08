7cd7ebce8553195bdd9fbdd86290b41c09f86ed8
Ivan Utkin <iutkin@ethz.ch>
2024-10-08 19:42:59 +0200

    Update lecture and homework instructions

--- slide-notebooks/deploy_notebooks.jl
+++ slide-notebooks/deploy_notebooks.jl
@@ -2,7 +2,7 @@ using Literate
 ## include Literate scripts starting with following 3 letters in the deploy
 incl = "l4_"
 ## Set `sol=true` to produce output with solutions contained and hints stripts. Otherwise the other way around.
-sol = false
+sol = true
 ##
 
 function replace_string(str)
