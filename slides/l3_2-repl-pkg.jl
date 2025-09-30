a9cc2e3945f5d3ae63af50b30af10aee59e7beaf
Ivan Utkin <iutkin@ethz.ch>
2025-09-30 11:52:53 +0200

    Update Julia version

--- slides/l3_2-repl-pkg.jl
+++ slides/l3_2-repl-pkg.jl
@@ -100,7 +100,7 @@ This can be achieved by typing entering the Pkg mode from the Julia REPL in the 
 ```julia-repl
 julia> ]
 
-(@v1.10) pkg> activate .
+(@v1.11) pkg> activate .
 
 (lectureXX) pkg> add Plots
 ```
