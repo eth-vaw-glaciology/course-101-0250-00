05a4f1cad40cd2b7d32c5d190b6eca09f1c2aaf0
Ivan Utkin <iutkin@ethz.ch>
2025-10-28 16:13:15 +0100

    Update wording

--- exercises/deploy_notebooks.jl
+++ exercises/deploy_notebooks.jl
@@ -1,6 +1,6 @@
 using Literate
 ## include Literate scripts starting with following letters in the deploy
-incl = "lecture7_"
+incl = "lecture4_"
 ## Set `sol=true` to produce output with solutions contained and hints stripts. Otherwise the other way around.
 sol = false
 ##
--- website/_literate/lecture4_ex1_web.jl
+++ website/_literate/lecture4_ex1_web.jl
@@ -67,7 +67,7 @@ md"""
 Repeat the steps from the Exercise 1 in Homework 3 to create the implicit time-dependent diffusion solver but in 2D. **Do not include advection yet.** Pay attention to add information relative to the second dimension whenever it's needed.
 
 
-Make a short animation showing the time evolution of the concentration field `C` during `nt = 100` physical time steps. The figure should contain 2 subplots, the first displaying the `heatmap` of the `C` field and the second the evolution of the by `nx` normalised iteration count:
+Make a short animation showing the time evolution of the concentration field `C` during `nt = 100` physical time steps. The figure should contain 2 subplots, the first displaying the `heatmap` of the `C` field and the second the evolution of the iteration count normalised by `nx`:
 
 ```julia
 # visualisation
