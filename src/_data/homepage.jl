d77741cfaec1f34486974371999fc2725c622ccc
Ivan Utkin <iutkin@ethz.ch>
2026-09-15 01:49:03 +0200

    Update layout

--- src/_data/homepage.jl
+++ src/_data/homepage.jl
@@ -2,10 +2,7 @@ Dict(
     "title" => @htl("Solving partial differential equations in parallel on GPUs"),
 
     # # add a disclaimer to the course webpage. Remove it if you dont want to include it.
-    "disclaimer" => md"""
-    This template will allow you to build a website like the **Computational thinking with Julia** class tought at MIT.
-    Use it to harness the power of Julia and Pluto in your own teaching and enhance students learning experience.
-    """,
+#     "disclaimer" => "",
 
     # Highlights the key features of your class to make it more engaging. Remove it if you dont want to include it.
     "highlights" => [
