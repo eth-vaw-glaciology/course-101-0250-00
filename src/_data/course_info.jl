3b963f5f2b211188d229366f64054d39cc54b2b2
Ludovic Raess <ludovic.rass@gmail.com>
2026-09-15 08:44:24 +0200

    VAW logo theme swap

--- src/_data/course_info.jl
+++ src/_data/course_info.jl
@@ -10,7 +10,8 @@ Dict(
         "Samuel Omlin" => "https://github.com/omlins"
     ],
     "institution" => "ETH Zürich",
-    "institution_logo" => "vaw_logo.png",
+    "institution_logo" => "vaw_logo_color.png",
+    "institution_logo_dark" => "vaw_logo_dark.png",
     "repo" => "https://github.com/eth-vaw-glaciology/course-101-0250-00",
     "moodle_url" => "https://moodle-app2.let.ethz.ch/course/view.php?id=28925",
 )
