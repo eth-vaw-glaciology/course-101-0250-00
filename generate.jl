a4f02420601bae984a3e937fb72278b80d857b86
Ivan Utkin <iutkin@ethz.ch>
2026-09-15 11:28:22 +0200

    Move all resources to external folder

--- generate.jl
+++ generate.jl
@@ -1,29 +1,8 @@
-SOLUTION_CUTOFF = 0
-LECTURES_PATH   = joinpath(@__DIR__, "lectures")
-
 import Pkg
 Pkg.activate("./pluto-deployment-environment")
 Pkg.instantiate()
 
-import PlutoSplitter
-
-# split the notebook into solution and 
-for path in readdir(LECTURES_PATH)
-    for file in readdir(joinpath(LECTURES_PATH, path))
-        filepath = joinpath(LECTURES_PATH, path, file)
-        lecture_id = parse(Int, filter(isnumeric, splitext(file)[1]))
-        
-        output_filename = joinpath(@__DIR__, "src", path, file)
-        type = lecture_id <= SOLUTION_CUTOFF ? "solution" : "statement"
-
-        @info "splitting notebook" file type
-
-        PlutoSplitter.split_notebook(filepath, type; output_filename)
-
-        # PlutoSplitter changes directory
-        cd(@__DIR__)
-    end
-end
+include("split.jl")
 
 import PlutoPages
 
--- src/logistics.md
+++ src/logistics.md
@@ -128,6 +128,9 @@ Enrolled ETHZ students will have to hand in on [Moodle]($(course_info["moodle_ur
 
 ## The use of large language models (LLMs)
 
+!!! warning
+    We will probably adjust these guidelines as the course develops
+
 - LLMs can be very helpful, but using them during class or to write your final project can prevent you from developing the skills the course is designed to teach.
 - We cannot stop you from using LLMs, but we strongly discourage ["vibe coding"](https://en.wikipedia.org/wiki/Vibe_coding). You are fully responsible for your code and results.
 - Your final project repository **must** include a section in the README stating which AI tools were used, for which tasks, and how they contributed to the project.
